3f020faec87d23fa2f912e2f1679cf3e21bfbd30
ef vl <efvl2022@gmail.com>
2026-01-30 20:33:27 +0100

    init commit

---
+++ src/main/java/com/evv/extbook/ExtbookApplication.java
@@ -0,0 +1,13 @@
+package com.evv.extbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evv/extbook/ExtbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evv.extbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
